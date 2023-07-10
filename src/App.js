b7e36b8a12c89a40ec7007fde9df23158d1fec55
Madhav Singh <45139112+madhavsingh203@users.noreply.github.com>
2023-07-10 11:45:49 +0000

    Update 13 files

--- src/App.js
+++ src/App.js
@@ -1,11 +1,6 @@
 import "./App.css";
 
 import Dashboard from "./components/Dashboard/Dashboard";
-import Graph from "./components/Graph/Graph";
-import BarChart from "./components/BarChart/BarChart";
-import Sidebar from "./components/Sidebar/Sidebar";
-import PieGraph from "./components/PieGraph/PieGraph";
-import PieGraphCard from "./components/PieGraph/PieGraphCard";
 import Sider from "./components/Sidebar/Sidebar";
 import Header from "./components/Header/Header";
 
